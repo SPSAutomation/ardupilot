d403292ba6cafb9e72b49f9a49adb6d7061c2636
Daniel Hadlow <daniel.hadlow@spsautomation.com>
2026-07-29 11:33:58 +1200

    Fixed incorrect usage of constrain_float()

--- libraries/AP_SpraySystem/AP_SpraySystem.cpp
+++ libraries/AP_SpraySystem/AP_SpraySystem.cpp
@@ -294,7 +294,7 @@ void AP_SpraySystem::flow_pid_step(uint32_t dt_ms)
 
         uint32_t new_pump_speed_us = pump->get_speed() + static_cast<int32_t>(correction);
 
-        constrain_float(new_pump_speed_us, PUMP_MIN_THROTTLE_PERIOD, PUMP_MAX_THROTTLE_PERIOD);
+        new_pump_speed_us = constrain_float(new_pump_speed_us, PUMP_MIN_THROTTLE_PERIOD, PUMP_MAX_THROTTLE_PERIOD);
 
         pump->set_speed(new_pump_speed_us);
     }
